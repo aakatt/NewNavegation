fdc1d9e15a843122d46513a1887819bb8226aa42
MatheusOlca <matheus.caldas2@etec.sp.gov.br>
2022-06-06 09:06:24 -0300

    First Commit

---
+++ app/src/main/java/com/example/nav/FragmentTwo.kt
@@ -0,0 +1,22 @@
+package com.example.nav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class FragmentTwo : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_two, container, false)
+
+        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_fragmentTwo_to_fragmentOne) }
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/nav/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.nav;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
